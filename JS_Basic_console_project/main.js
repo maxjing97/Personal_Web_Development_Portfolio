fd6d9975ddbc7b6564bcd1f9693be08dedeffc5e
Max Jing <maxjing97@gmail.com>
2022-01-14 01:38:03 -0600

    main javascript code finished.

--- JS_Basic_console_project/main.js
+++ JS_Basic_console_project/main.js
@@ -5,6 +5,8 @@ let category = ""; //global variable to store the category of astrological sign 
 categorizeAstrologicalSign = (input) => {
     
     input = input.toLowerCase(); //converts all signs to lowercase
+
+    //astrological signs are categorized into the classic fire, water, earth, and air categories
     
    if(input === "aries" || input === "leo" || input === "sagittarius") {
     category = "fire";
@@ -27,22 +29,77 @@ categorizeAstrologicalSign = (input) => {
    }
 }
 
-//function to generate a random number from 1 to 10 (inclusive on both ends)/
-generateRandomNumber10 = () => {
-    return Math.floor(Math.random() * 10 + 1);
+
+//function to generate a random number from a number to another (inclusive on both ends)/
+generateRandomNumberRange = (start, end) => {
+    return Math.floor(Math.random() * (end - start + 1) + start);
 }
 
-//function to generate a random number from 1 to 3 (inclusive on both ends)/
-generateRandomNumber3 = () => {
-    return Math.floor(Math.random() * 3 + 1);
+
+//here, we set three possible formats an astrological horoscope can take
+generateHoroscope = () => {
+    const value = generateRandomNumberRange(1, 3); //choses which message format the horoscope will take.
+    if(value === 1) {
+        return message1();
+    }
+    if(value === 2) {
+        return message2();
+    }
+    if(value === 3) {
+        return message3();
+    }
 }
 
-//here, we set three possible formats an astrological can take
+//object to store possible words based on the category of astrological sign
+const words = {
+    traits: {
+        water: ["imagination", "emotional intelligence", "inventiveness"],
+        fire: ["bravery", "confidence", "independent thinking"],
+        air: ["social skills", "intelligence", "learning skills"],
+        earth: ["practical knowledge", "attention to detail", "consistency"]
+    },
+    
+    goals: ["deepest desires", "dreams", "relationship goals", "greatest passion"]
+};
+
+//finds a random trait to add to the horoscope based on the category of the astrological sign and the words object.
+generateRandomWords = () => {
+    let toReturn = []; //array to be returned. The first value is the trait and the second value is the goal.
+    
+    //iterating over sub-properties of an object: for..in loop
+    //returns when the key matches the category of the astrological sign.
+    for(const sign in words.traits) {
+        const arraylength = words.traits[sign].length; //length of the sign category's trait array.
+        if(sign === category) {
+            //adds a random word from the sign category's trait array.
+            toReturn.push(words.traits[sign][generateRandomNumberRange(0, (arraylength-1) )]); 
+        }
+    }
+
+    //pushes random goal to the array
+
+    toReturn.push(words.goals[generateRandomNumberRange(0, (words.goals.length-1))  ] );
+    return toReturn;
+}
 
 
+//1st possible message function
+const message1 = () => {
+    return `This week, you will be guided by your ${generateRandomWords()[0]} to pursue your goals. 
+    You will advance your ${generateRandomWords()[1]}.`;
+};
 
+//2nd possible message function
+const message2 = () => {
+    return `Despite your disappointments from last week, this week, your ${generateRandomWords()[0]} will allow you to
+    fulfill, at least partially, your ${generateRandomWords()[1]}.`;
+};
 
-
+//3rd possible message function
+const message3 = () => {
+    return `You are on track, due to your ${generateRandomWords()[0]}, to achieving your 
+    ${generateRandomWords()[1]}.`;
+};
 
 
 
@@ -58,14 +115,13 @@ generateRandomNumber3 = () => {
 //          All code below are inputs and outputs to and from the terminal (using the node.js environment).
 const prompt = require('prompt-sync')();
 
-console.log("Horoscope finder for your astrological sign: ")
-console.log("possible signs are spelled:\n Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius and Pisces.\n\n");
+console.log("\tHoroscope finder for your astrological sign: ")
+console.log("The 12 possible astrological signs are spelled:\n Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius and Pisces.\n\n");
 
 //try inputing as long as the input isn't valid
 do {
     AstrologicalSign = prompt('Enter your astrological sign: ');
     
 } while (!categorizeAstrologicalSign(AstrologicalSign))
-
-
-  
+//testing final output
+console.log(generateHoroscope() + "\n\n");
