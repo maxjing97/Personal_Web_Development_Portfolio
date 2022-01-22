31c4dc4b370c100546a95d075a76600a9f0a3380
Max Jing <maxjing97@gmail.com>
2022-01-21 21:23:26 -0600

    Changed paragraph

--- Horoscope_generator_web_app/script.js
+++ Horoscope_generator_web_app/script.js
@@ -4,12 +4,15 @@ const astrologicalSigns = document.getElementById("astrological_signs");
 //generate click event for each button.
 for (let i = 0; i < astrologicalSigns.childNodes.length; i++) {
     let element = astrologicalSigns.childNodes[i]; //set the element at each iteration to be the sign at a certain index in the list of nodes
-    element.addEventListener('click', (e) => { //set the sign to be the current element's id;
-        returnResult(e.target.id);  //trigger the function to change the text on the screen based on the clicked button's id;
+    element.addEventListener('click', (e) => { //set the sign to be the current element's id. Each element iterated through is a li in the ul list.
+        returnResult(e.target);  //trigger the function to change the text on the screen based on the clicked button's id;
     });
 }
 
-const returnResult = (sign_id) => {
-    document.getElementById("result").innerText = generateHoroscope(sign_id); //send the generated message to the user
+const returnResult = (element) => {
+    let paragraph = document.getElementById("result");
+
+    paragraph.style.display = 'block'; //displays hidden paragraph to show the message.
+    paragraph.innerText = generateHoroscope(element.id); //send the generated message to the user
 }
 
