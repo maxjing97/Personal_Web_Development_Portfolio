7e5a813ccb85838e40e1919aa06a6eabe1bb5fbb
Max Jing <maxjing97@gmail.com>
2022-02-27 00:16:04 -0600

    basic format set up

--- connect4_app/src/index.js
+++ connect4_app/src/index.js
@@ -1,25 +1,74 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 
-const basic_element = (
-    
-    <div  id='wrapper'>
-        <h1>hello world</h1>
-    </div>
-    
-);
+let diskClicked = ""; //string storing the disk clicked
 
-//react components must be capitalized
-class MyComponent extends React.Component {
+class RedDisk extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {clicked : false};
+        this.state = { 
+            style: {
+                backgroundColor: "red"
+            }  
+        };
+    }
+
+    clicked() {
+        diskClicked = "red";
+        console.log(diskClicked);
+    }
+
+    hovered() {
+        this.setState({style: {
+            backgroundColor: "red"
+        }});
+    }
+    render() {
+        return (
+            <div>
+                <button id='red' onClick={this.clicked}  style={this.state.style}>
+                Red
+                </button>
+            </div>
+        );
+    }
+};
+
+class BlackDisk extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            style: {
+                backgroundColor: "black"
+            }  
+        };
+    }
+
+    clicked() {
+        diskClicked = "black";
+        console.log(diskClicked);
     }
 
     render() {
-        return basic_element;   
+        return (
+            <div>
+                <button id='black' onClick={this.clicked} style={this.state.style}>Black</button>
+            </div>
+        );
     }
-}
+};
+
+//component encompassing all the two disks
+class Disks extends React.Component {
+    render() {
+        return (
+            <div>
+                <RedDisk/>
+                <BlackDisk/>
+            </div>
+        );
+    }
+};
 
 
-ReactDOM.render(<MyComponent/>, document.getElementById('app'));
+ReactDOM.render(<Disks/>, document.getElementById('disks'));
